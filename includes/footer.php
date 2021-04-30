a3a8ece8544681010bb17123083a006da7c1a59b
Santanu Ghosh <53248899+santanugh@users.noreply.github.com>
2021-04-30 11:48:32 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="mt-5 text-center col-md-12">
+                &copy; 2021 Library Management System |<a href="index.php" target="_blank"  > Designed by : Santanu, Ramsha, Sriya, Ahaan, Ningthoujam</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
